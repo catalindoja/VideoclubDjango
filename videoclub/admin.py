d53b18c2f9d83236ecb8e5f093a54607491eb23d
catalindoja <cdojamihai@gmail.com>
2022-03-15 11:59:00 +0100

    Fichero urls creado y listo para hacer vistas y potencialmente forms.

--- videoclub/admin.py
+++ videoclub/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from videoclub import models
 
 # Register your models here.
+admin.site.register(models.Videoclub)
+admin.site.register(models.Pelicula)
+admin.site.register(models.Cliente)
+admin.site.register(models.Empleado)
---
+++ videoclub/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.views.generic import DetailView, ListView, UpdateView
+from videoclub.models import Videoclub, Pelicula, Empleado, Cliente
+#from myrestaurants.views import RestaurantCreate, DishCreate, RestaurantDetail, review, LoginRequiredCheckIsOwnerUpdateView
+
+app_name = "myrestaurants"
+
+urlpatterns = [
+
+]
