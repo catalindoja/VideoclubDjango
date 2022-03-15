ac23533b29c761d4a7caf2bb487f8afdddd8ea91
catalindoja <cdojamihai@gmail.com>
2022-03-15 11:39:40 +0100

    modelos de videoclub y empleados

--- videoclub/models.py
+++ videoclub/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Videoclub(models.Model):
+    idVideoclub = models.IntegerField(primary_key=True)
+    nombre = models.TextField(null=False, blank=False)
+
+    def __unicode__(self):
+        return u"%s" % self.nombre
+
+
+class Empleado(models.Model):
+    idEmpleado = models.IntegerField(primary_key=True)
+    nombre = models.TextField(null=False, blank=False)
+    apellido = models.TextField(null=False, blank=False)
+    idVideoclub = models.ForeignKey(Videoclub, default=1, on_delete=models.PROTECT)
+
+    def __unicode__(self):
+        return u"%s" % self.nombre
